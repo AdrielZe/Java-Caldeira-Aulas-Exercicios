b9f5feb7a60e4c3bb1d50171cabf9029447f1f4f
AdrielZe <121925084+AdrielZe@users.noreply.github.com>
2023-09-16 16:33:23 -0300

    Adicionando exercicios do banco

--- Aulas/aula_3_conta_bancaria/Main.java
+++ Aulas/aula_3_conta_bancaria/Main.java
@@ -2,13 +2,22 @@ package aula_3_conta_bancaria;
 
 public class Main {
     public static void main(String[] args) {
-        BankAccount contaDoAdriel = new BankAccount("Adriel",1500);
-        BankAccount outraConta = new BankAccount("Rodrigo",500);
+        TaxaMensalidade taxa = new TaxaMensalidade();
 
-        contaDoAdriel.transference(2000,outraConta);
+       BankAccount contaDoAdriel = new BankAccount("Adriel","85916510063");
+       BankAccount contaDoJoao = new BankAccount("João","92571433091");
+
+        contaDoAdriel.deposit(4000);
+        contaDoAdriel.withdraw(2000);
+        contaDoAdriel.transference(1000,contaDoJoao);
         System.out.println(contaDoAdriel.getBalance());
-        System.out.println(outraConta.getBalance());
 
-        System.out.println(contaDoAdriel.currentTime.getHour());
+        contaDoAdriel.closeAccount();
+
+        contaDoAdriel.deposit(4000);
+        contaDoAdriel.withdraw(2000);
+        contaDoAdriel.transference(1000,contaDoJoao);
+        System.out.println(contaDoAdriel.getBalance());
+
     }
-}
+ }
